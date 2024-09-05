da3d3f1d9df324dec7bbe637fa17d9bda0f015fa
Lars Gersmann <lars.gersmann@gmail.com>
2024-09-05 07:43:53 +0200

    wip(@pnpmkambrium/core): continued try/eval on prepare-commit-message.sh

--- packages/npm/core/bin/prepare-commit-message.sh
+++ packages/npm/core/bin/prepare-commit-message.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
+set -e pipefail
+
 # GUM='docker run  --pull=always -ti --rm pnpmkambrium/gum'
-GUM='docker run  -ti --rm pnpmkambrium/gum'
+# GUM='docker run  -ti --rm pnpmkambrium/gum'
+function gum() {
+  docker run -ti --rm pnpmkambrium/gum "$@"
+}
 
 # COMMIT_TYPES=("fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
-set -x
-COMMIT_TYPE=$(docker run  -ti --rm pnpmkambrium/gum choose --header "Select the type of change that you're committing:" "a" "b")
+# set -x
+# COMMIT_TYPE=$(docker run  -ti --rm pnpmkambrium/gum choose --header "Select the type of change that you're committing:" "a" "b")
+
+# shopt -s lastpipe
+# gum choose --header "Select the type of change that you're committing:" "a" "b" | read COMMIT_TYPE
+
+gum choose --header "Select the type of change that you're committing:" "a" "b"
+gum choose --header "next option" "xxx" "yyy"
 
 echo "COMMIT_TYPE=$COMMIT_TYPE"
